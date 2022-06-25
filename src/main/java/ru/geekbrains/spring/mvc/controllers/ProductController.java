ae870020689ffce7ca72a3381cf7d7069b69791d
unknown <zlovred12@gmail.com>
2022-06-25 18:41:22 +0300

    'add_product'

--- src/main/java/ru/geekbrains/spring/mvc/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/spring/mvc/controllers/ProductController.java
@@ -3,10 +3,7 @@ package ru.geekbrains.spring.mvc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.spring.mvc.model.Product;
 import ru.geekbrains.spring.mvc.services.ProductService;
 
@@ -27,5 +24,11 @@ public class ProductController {
         List<Product> list = productService.getAllProducts();
         model.addAttribute("products", list);
         return "form";
+    }
+    @GetMapping("/add_product")
+    public String addProduct(@RequestParam String title, @RequestParam int cost){
+        Product p = new Product(1l, title, cost);
+        productService.addProduct(p);
+        return "redirect:/products";
     }
 }
