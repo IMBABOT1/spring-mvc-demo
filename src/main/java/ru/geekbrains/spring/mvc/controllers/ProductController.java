8c843f65f74bc3cca6fcea6581debc3002e29b7c
unknown <zlovred12@gmail.com>
2022-06-25 16:38:05 +0300

    temp

--- src/main/java/ru/geekbrains/spring/mvc/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/spring/mvc/controllers/ProductController.java
@@ -1,7 +1,16 @@
 package ru.geekbrains.spring.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProductController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(){
+        return "Hello";
+    }
 }
